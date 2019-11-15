a16486737ffdce9eb6b8b0c934d166ba7e230383
k-kawajiri <kawanojieee@gmail.com>
2019-11-15 13:37:52 +0900

    [add]gistアップ用テストコード

---
+++ app/src/main/java/com/k/k/playground/gisttest/Test1.kt
@@ -0,0 +1,11 @@
+package com.k.k.playground.gisttest
+
+class Test1 {
+    fun hoge(){
+        // test用メソッドその1
+    }
+
+    fun fuga(){
+        // test用メソッドその2
+    }
+}
---
+++ app/src/main/java/com/k/k/playground/gisttest/Test2.kt
@@ -0,0 +1,7 @@
+package com.k.k.playground.gisttest
+
+class Test2 {
+    fun hoge(){
+        // test用メソッド
+    }
+}
